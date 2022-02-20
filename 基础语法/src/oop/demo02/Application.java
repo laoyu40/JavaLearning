b9106442b26763bf6dfc9bebdcb1096ebdfa1855
laoyu40 <haozheng40@126.com>
2022-02-20 10:07:00 +0800

    OK

---
+++ 基础语法/src/oop/demo02/Application.java
@@ -0,0 +1,4 @@
+package oop.demo02;
+
+public class Application {
+}
--- 基础语法/src/oop/demo02/Pet.java
+++ 基础语法/src/oop/demo02/Pet.java
@@ -1,5 +1,9 @@
 package oop.demo02;
 
 public class Pet {
-
+    String name;
+    int age;
+    public void shout() {
+        System.out.println("叫了一声");
+    }
 }
